ab3d04cd8e987da7b3ff6913f8deaabfade216da
naqeebghazi <naqeebghazi@outlook.com>
2023-11-09 11:40:30 +0000

    images

--- bash4git.sh
+++ bash4git.sh
@@ -5,4 +5,4 @@ git commit -am 'images'
 git push
 
 
-ssh -i /Users/nghazi/Downloads/ec2-nghazi.pem ubuntu@ec2-18-132-15-167.eu-west-2.compute.amazonaws.com
+# ssh -i /Users/nghazi/Downloads/ec2-nghazi.pem ubuntu@ec2-18-132-15-167.eu-west-2.compute.amazonaws.com
